4c562a195c8811d179c5985d16926f072c8400c8
Kovteba Dima <kovteba@gmail.com>
2020-03-21 20:04:35 +0200

    Update RequestDAOImpl.class

--- src/main/java/ua/nure/kovteba/finaltask/dao/request/RequestDAO.java
+++ src/main/java/ua/nure/kovteba/finaltask/dao/request/RequestDAO.java
@@ -7,9 +7,12 @@ import java.util.List;
 
 public interface RequestDAO {
 
-    void createRequest(Request request);
+    Long createRequest(Request request);
 
     List<Request> getAllRequestByStatus(RequestStatus requestStatus);
-    
+
+    Request getRequestById(Long id);
+
+    void changeStatusRequestById(Long id, RequestStatus requestStatus);
 
 }
--- src/main/java/ua/nure/kovteba/finaltask/dao/request/RequestDAOImpl.java
+++ src/main/java/ua/nure/kovteba/finaltask/dao/request/RequestDAOImpl.java
@@ -1,4 +1,170 @@
 package ua.nure.kovteba.finaltask.dao.request;
 
-public class RequestDAOImpl {
+import ua.nure.kovteba.finaltask.connection.Connect;
+import ua.nure.kovteba.finaltask.entity.Request;
+import ua.nure.kovteba.finaltask.entity.User;
+import ua.nure.kovteba.finaltask.enumlist.CarClass;
+import ua.nure.kovteba.finaltask.enumlist.RequestStatus;
+import ua.nure.kovteba.finaltask.enumlist.Role;
+import ua.nure.kovteba.finaltask.util.Serialization;
+
+import java.io.IOException;
+import java.sql.*;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.logging.Logger;
+
+public class RequestDAOImpl implements RequestDAO {
+
+    //Create logger
+    private static Logger LOG = Logger.getLogger(RequestDAOImpl.class.getName());
+
+    //set connection
+    private static Connection conn = Connect.connect();
+
+    //
+    private static Serialization serialization = new Serialization();
+
+    //create statement
+    private static Statement smtp;
+
+    static {
+        try {
+            smtp = conn.createStatement();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Insert new request in data base
+     * @param request
+     * @return id new user in data base Long
+      */
+    @Override
+    public Long createRequest(Request request) {
+        Long idNewRequest = null;
+        //SQL query for create new user
+        String insert = "INSERT INTO " +
+                "requests (driver_id, car_class, load_capacity, seats, luggage_compartment, air_conditioning, navigator, request_status) " +
+                "VALUES (?, ?, ?, ?, ?, ?, ?, ?);";
+        //Create PreparedStatement in try with resources
+        try (PreparedStatement preparedStatement = conn.prepareStatement(insert,
+                PreparedStatement.RETURN_GENERATED_KEYS);) {
+            //set value in insert string
+            preparedStatement.setString(1, serialization.entityToString(request.getDriver()));
+            preparedStatement.setString(2, request.getCarClass().getClassValue());
+            preparedStatement.setInt(3, request.getLoadCapacity());
+            preparedStatement.setInt(4, request.getSeats());
+            preparedStatement.setBoolean(5, request.getLuggageCompartment());
+            preparedStatement.setBoolean(6, request.getAirConditioning());
+            preparedStatement.setBoolean(7, request.getNavigator());
+            preparedStatement.setString(8, request.getRequestStatus().getRequestSatus());
+            //execute insert to table
+            preparedStatement.executeUpdate();
+            ResultSet resultSet = preparedStatement.getGeneratedKeys();
+            if (resultSet.next()) {
+                idNewRequest = resultSet.getLong(1);
+            }
+            LOG.info("new request added successfully");
+        } catch (SQLException | IOException e) {
+            e.printStackTrace();
+        }
+        return idNewRequest;
+    }
+
+    /**
+     * Return all request by status : OPEN, CLOSED
+     * @param requestStatus
+     * @return List<Request>
+     */
+    @Override
+    public List<Request> getAllRequestByStatus(RequestStatus requestStatus) {
+        //create return list
+        List<Request> allRequestBySatus = new ArrayList();
+        //SQL query for select request by status
+        String selectAllByRole = "SELECT * FROM requests WHERE request_status = '" + requestStatus + "';";
+        //Create ResultSet in try with resources
+        try (ResultSet rs = smtp.executeQuery(selectAllByRole);) {
+            while (rs.next()) {
+                Request request = new Request();
+                request.setId(rs.getLong(1));
+                request.setDriver((User) serialization.fromString(rs.getString(8)));
+                request.setCarClass(CarClass.findCarClass(rs.getString(3)));
+                request.setLoadCapacity(rs.getInt(4));
+                request.setSeats(rs.getInt(7));
+                request.setLuggageCompartment(rs.getBoolean(5));
+                request.setAirConditioning(rs.getBoolean(2));
+                request.setNavigator(rs.getBoolean(6));
+                request.setRequestStatus(RequestStatus.findRequestStatus(rs.getString(9)));
+                allRequestBySatus.add(request);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        //return all users by role
+        return allRequestBySatus;
+    }
+
+    /**
+     * Get request by Long id
+     * @param id request
+     * @return request
+     */
+    @Override
+    public Request getRequestById(Long id) {
+        Request request = new Request();
+        //SQL query for select request by id
+        String selectUserById = "SELECT * FROM requests " +
+                "WHERE id = '" + id + "';";
+        //Create ResultSet in try with resources
+        try (ResultSet rs = smtp.executeQuery(selectUserById);) {
+            while (rs.next()) {
+                request.setId(rs.getLong(1));
+                request.setDriver((User) serialization.fromString(rs.getString(8)));
+                request.setCarClass(CarClass.findCarClass(rs.getString(3)));
+                request.setLoadCapacity(rs.getInt(4));
+                request.setSeats(rs.getInt(7));
+                request.setLuggageCompartment(rs.getBoolean(5));
+                request.setAirConditioning(rs.getBoolean(2));
+                request.setNavigator(rs.getBoolean(6));
+                request.setRequestStatus(RequestStatus.findRequestStatus(rs.getString(9)));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        //return request by id
+        return request;
+    }
+
+    /**
+     * Change request status : OPEN, CLOSED
+     * @param id
+     * @param requestStatus
+     */
+    @Override
+    public void changeStatusRequestById(Long id, RequestStatus requestStatus) {
+        //SQL query for update status request by id
+        String changeStatusRequestById =
+                "UPDATE requests SET request_status = ? WHERE id = ?;";
+        //Create ResultSet in try with resources
+        PreparedStatement preparedStmt = null;
+        try {
+            preparedStmt = conn.prepareStatement(changeStatusRequestById);
+            preparedStmt.setString(1, requestStatus.getRequestSatus());
+            preparedStmt.setLong(2, id);
+            preparedStmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
