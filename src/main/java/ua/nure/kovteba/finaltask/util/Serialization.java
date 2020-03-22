572c48858630f0a358338c6fa917c28404cbab95
Kovteba Dima <kovteba@gmail.com>
2020-03-22 12:38:50 +0200

    Add Serialization.java with two method: entityToString, fromString.

---
+++ src/main/java/ua/nure/kovteba/finaltask/util/Serialization.java
@@ -0,0 +1,26 @@
+package ua.nure.kovteba.finaltask.util;
+
+import java.io.*;
+import java.util.Base64;
+
+public class Serialization {
+
+    public String entityToString(Serializable o) throws IOException {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ObjectOutputStream oos = new ObjectOutputStream(baos);
+        oos.writeObject(o);
+        oos.close();
+        return Base64.getEncoder().encodeToString(baos.toByteArray());
+    }
+
+    public Object fromString(String s) throws IOException,
+            ClassNotFoundException {
+        byte[] data = Base64.getDecoder().decode(s);
+        ObjectInputStream ois = new ObjectInputStream(
+                new ByteArrayInputStream(data));
+        Object o = ois.readObject();
+        ois.close();
+        return o;
+    }
+
+}
