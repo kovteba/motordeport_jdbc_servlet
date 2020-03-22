d83d7c771b2247ae42167afad7af4e9ffb034ab2
Kovteba Dima <kovteba@gmail.com>
2020-03-22 18:28:58 +0200

    Add getAllFlight method tom FlightDAOImpl.class

--- src/main/java/ua/nure/kovteba/finaltask/dao/flight/FlightDAOImpl.java
+++ src/main/java/ua/nure/kovteba/finaltask/dao/flight/FlightDAOImpl.java
@@ -1,11 +1,17 @@
 package ua.nure.kovteba.finaltask.dao.flight;
 
+import ua.nure.kovteba.finaltask.entity.Car;
+import ua.nure.kovteba.finaltask.entity.User;
+import ua.nure.kovteba.finaltask.enumlist.FlightStatus;
+import ua.nure.kovteba.finaltask.enumlist.Role;
 import ua.nure.kovteba.finaltask.util.Connect;
 import ua.nure.kovteba.finaltask.entity.Flight;
 import ua.nure.kovteba.finaltask.util.Serialization;
 
 import java.io.IOException;
 import java.sql.*;
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
 
@@ -33,11 +39,12 @@ public class FlightDAOImpl implements FlightDAO {
 
     @Override
     public Long createFlight(Flight flight) {
+        LOG.info("CREATE FLIGHT");
         Long idNewRequest = null;
-        //SQL query for create new user
+        //SQL query for create new flight
         String insert = "INSERT INTO " +
                 "flights (flight_number, flight_status, car_id, driver_id, start_date, end_date) " +
-                "VALUES (?, ?, ?, ?, ?, ?, ?, ?);";
+                "VALUES (?, ?, ?, ?, ?, ?);";
         try (PreparedStatement preparedStatement = conn.prepareStatement(insert,
                 PreparedStatement.RETURN_GENERATED_KEYS);) {
             //set value in insert string
@@ -46,14 +53,14 @@ public class FlightDAOImpl implements FlightDAO {
             preparedStatement.setString(3, serialization.entityToString(flight.getCar()));
             preparedStatement.setString(4, serialization.entityToString(flight.getDriver()));
             preparedStatement.setString(5, flight.getStartDate().toString());
-            preparedStatement.setString(5, flight.getEndDate().toString());
+            preparedStatement.setString(6, flight.getEndDate().toString());
             //execute insert to table
             preparedStatement.executeUpdate();
             ResultSet resultSet = preparedStatement.getGeneratedKeys();
             if (resultSet.next()) {
                 idNewRequest = resultSet.getLong(1);
             }
-            LOG.info("new request added successfully");
+            LOG.info("NEW FLIGHT SDDED SUCCESSFULY");
         } catch (SQLException | IOException e) {
             e.printStackTrace();
         }
@@ -62,6 +69,31 @@ public class FlightDAOImpl implements FlightDAO {
 
     @Override
     public List<Flight> getAllFlight() {
-        return null;
+        LOG.info("GET ALL FLIGHT");
+        List<Flight> flightList = new ArrayList<>();
+        //SQL query for create new flight
+        String selectAll = "SELECT * FROM flights;";
+        //Create ResultSet in try with resources
+        try (ResultSet rs = smtp.executeQuery(selectAll);) {
+            while (rs.next()) {
+                Flight newFlight = new Flight();
+                newFlight.setId(rs.getLong(1));
+                newFlight.setEndDate(ZonedDateTime.parse(rs.getString(2)));
+                newFlight.setFlightNumber(rs.getInt(3));
+                newFlight.setFlightStatus(FlightStatus.findFlightStatus(rs.getString(4)));
+                newFlight.setStartDate(ZonedDateTime.parse(rs.getString(5)));
+                newFlight.setCar((Car) serialization.fromString(rs.getString(6)));
+                newFlight.setDriver((User) serialization.fromString(rs.getString(7)));
+                flightList.add(newFlight);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        //return all flight
+        return flightList;
     }
 }
