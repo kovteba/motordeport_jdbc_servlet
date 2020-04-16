7005ba72e03cad03e8fa81ded5d0b8dcdb210ab1
Kovteba Dima <kovteba@gmail.com>
2020-04-16 22:48:07 +0300

    Add CompareFlightByStatus in util

---
+++ src/main/java/ua/nure/kovteba/finaltask/util/CompareFlightByStatus.java
@@ -0,0 +1,25 @@
+package ua.nure.kovteba.finaltask.util;
+
+import ua.nure.kovteba.finaltask.entity.Flight;
+import ua.nure.kovteba.finaltask.enumlist.FlightStatus;
+
+import java.util.Comparator;
+
+public class CompareFlightByStatus  implements Comparator<Flight> {
+
+    private FlightStatus status;
+
+    public CompareFlightByStatus(FlightStatus status) {
+        this.status = status;
+    }
+
+    @Override
+    public int compare(Flight o1, Flight o2) {
+        if (o2.getFlightStatus().getStatusValue().equals(status.getStatusValue())) {
+            return 1;
+        } else {
+            return -1;
+        }
+    }
+
+}
