c7579ba57417e311378846aa9895eab48bdac09c
Kovteba Dima <kovteba@gmail.com>
2020-04-17 17:57:38 +0300

    Clean CarDAOImplTest.java

--- src/test/java/ua/nure/kovteba/finaltask/dao/car/CarDAOImplTest.java
+++ src/test/java/ua/nure/kovteba/finaltask/dao/car/CarDAOImplTest.java
@@ -1,90 +1,112 @@
 package ua.nure.kovteba.finaltask.dao.car;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import ua.nure.kovteba.finaltask.dao.carbrand.CarBrandDAOImpl;
 import ua.nure.kovteba.finaltask.entity.Car;
-import ua.nure.kovteba.finaltask.entity.CarBrand;
 import ua.nure.kovteba.finaltask.enumlist.CarClass;
 import ua.nure.kovteba.finaltask.enumlist.CarStatus;
 import ua.nure.kovteba.finaltask.enumlist.CarTechnicalStatus;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class CarDAOImplTest {
 
-    private static CarDAOImpl carDAO;
+    private static final CarDAOImpl CAR_DAO;
 
-    private static CarBrandDAOImpl carBrandDAO;
+    private static final CarBrandDAOImpl CAR_BRAND_DAO;
+
+    private static Long idNewCar;
 
     static {
-        carDAO = new CarDAOImpl();
-        carBrandDAO = new CarBrandDAOImpl();
+        CAR_DAO = new CarDAOImpl();
+        CAR_BRAND_DAO = new CarBrandDAOImpl();
     }
 
     @Test
     void createCar() {
         Car car = new Car();
-        car.setCarBrand(carBrandDAO.getCarBrandByBrandValue("MAN"));
+        car.setCarBrand(CAR_BRAND_DAO.getCarBrandByBrandValue("MAN"));
         car.setCarClass(CarClass.COMFORT);
-        car.setCarNumber("AX6598A");
+        car.setCarNumber("AX6138A");
         car.setLoadCapacity(1000);
         car.setSeats(20);
         car.setLuggageCompartment(true);
         car.setAirConditioning(true);
         car.setNavigator(false);
         car.setCarTechnicalStatus(CarTechnicalStatus.GOOD);
-        car.setCarStatus(CarStatus.BUSY);
-        Long idNewCar = carDAO.createCar(car);
+        car.setCarStatus(CarStatus.FREE);
+        idNewCar = CAR_DAO.createCar(car);
+        System.out.println("New id from DB : " + idNewCar);
     }
 
     @Test
     void getCarById() {
-        System.out.println(carDAO.getCarById(1L).toString());
+        System.out.println(CAR_DAO.getCarById(26L).toString() + "\n");
     }
 
     @Test
     void changeCarTechnicalStatus() {
-        carDAO.changeCarTechnicalStatus(1L, CarTechnicalStatus.REPAIRS);
+        System.out.println("Old technical status : " +
+                CAR_DAO.getCarById(26L).getCarTechnicalStatus().getCarTechnicalStatusValue());
+        CAR_DAO.changeCarTechnicalStatus(26L, CarTechnicalStatus.REPAIRS);
+        System.out.println("New technical status : " +
+                CAR_DAO.getCarById(26L).getCarTechnicalStatus().getCarTechnicalStatusValue()
+                + "\n");
     }
 
     @Test
     void changeCarStatus() {
-        carDAO.changeCarStatus(1L, CarStatus.BUSY);
+        System.out.println("Old status : " + CAR_DAO.getCarById(26L).getCarStatus().getCarStatusValue());
+        CAR_DAO.changeCarStatus(26L, CarStatus.BUSY);
+        System.out.println("New status : " + CAR_DAO.getCarById(26L).getCarStatus().getCarStatusValue() + "\n");
     }
 
     @Test
     void getListCarFreeAndGood() {
+        System.out.println("List cars with values FREE status and GOOD technical status");
+        for (Car car : CAR_DAO.getListCarFreeAndGood()) {
+            System.out.println(car.getId() + " "
+                    + car.getCarStatus().getCarStatusValue() + " "
+                    + car.getCarTechnicalStatus().getCarTechnicalStatusValue());
+        }
+        System.out.println("\n");
     }
 
     @Test
     void getAllCars() {
-        List<Car> list = carDAO.getAllCars();
-        for (Car car : list){
+        System.out.println("List all cars");
+        for (Car car : CAR_DAO.getAllCars()) {
             System.out.println(car.toString());
         }
+        System.out.println("\n");
     }
 
     @Test
     void changeCarInformation() {
-        Car car = new Car();
-        car.setId(1L);
-        car.setCarBrand(carBrandDAO.getCarBrandByBrandValue("MAN"));
-        car.setCarClass(CarClass.COMFORT);
-        car.setCarNumber("!!!!!!!!!!!!");
-        car.setLoadCapacity(1000);
-        car.setSeats(20);
-        car.setLuggageCompartment(true);
-        car.setAirConditioning(true);
+        System.out.println("Change car information");
+        Car car = CAR_DAO.getCarById(26L);
+        System.out.println("Old value : " + car.toString());
+        car.setCarBrand(CAR_BRAND_DAO.getCarBrandByBrandValue("MAN"));
+        car.setCarClass(CarClass.ECONOMY);
+        car.setCarNumber("DC987H");
+        car.setLoadCapacity(500);
+        car.setSeats(10);
+        car.setLuggageCompartment(false);
+        car.setAirConditioning(false);
         car.setNavigator(false);
         car.setCarTechnicalStatus(CarTechnicalStatus.GOOD);
-        car.setCarStatus(CarStatus.BUSY);
-        carDAO.changeCarInformation(car);
+        car.setCarStatus(CarStatus.FREE);
+        CAR_DAO.changeCarInformation(car);
+        System.out.println("Old value : " + CAR_DAO.getCarById(26L).toString() + "\n");
 
     }
 
     @Test
     void deleteCarById() {
+        System.out.println("Delete car by id");
+        CAR_DAO.deleteCarById(26L);
     }
 }
