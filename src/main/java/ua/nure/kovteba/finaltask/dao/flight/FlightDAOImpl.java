fac23591b477714be3e3bf02313aa04e819b27f2
Kovteba Dima <kovteba@gmail.com>
2020-04-17 18:48:24 +0300

    Clean FlightDAOImpl and add Doc

--- src/main/java/ua/nure/kovteba/finaltask/dao/flight/FlightDAOImpl.java
+++ src/main/java/ua/nure/kovteba/finaltask/dao/flight/FlightDAOImpl.java
@@ -14,10 +14,14 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
 
+/**
+ * Data Access Object for Flight entity
+ * Methods: createFlight, getAllFlight, getAllFlightByDriver, changeFlightStatus
+ */
 public class FlightDAOImpl implements FlightDAO {
 
     //Create logger
-    private static Logger log = Logger.getLogger(FlightDAOImpl.class.getName());
+    private static final Logger LOG = Logger.getLogger(FlightDAOImpl.class.getName());
 
     //set connection
     private static final Connection CONNECT = Connect.connect();
@@ -36,9 +40,15 @@ public class FlightDAOImpl implements FlightDAO {
         }
     }
 
+    /**
+     * Create new flight
+     *
+     * @param flight entity
+     * @return id new Flight in database
+     */
     @Override
     public Long createFlight(Flight flight) {
-        log.info("Create flight --> " + flight.toString() + " ....");
+        LOG.info("Create flight --> " + flight.toString() + " ....");
         Long idNewRequest = null;
         //SQL query for create new flight
         String insert = "INSERT INTO " +
@@ -60,17 +70,22 @@ public class FlightDAOImpl implements FlightDAO {
             if (resultSet.next()) {
                 idNewRequest = resultSet.getLong(1);
             }
-            log.info("New request with id == " + idNewRequest + ", added successfully!");
+            LOG.info("New request with id == " + idNewRequest + ", added successfully!");
         } catch (SQLException | IOException e) {
             e.printStackTrace();
-            log.warning("Same problem in \"createFlight\" method");
+            LOG.warning("Same problem in \"createFlight\" method" + this.getClass());
         }
         return idNewRequest;
     }
 
+    /**
+     * Return all flights from database
+     *
+     * @return List<Flight>
+     */
     @Override
     public List<Flight> getAllFlight() {
-        log.info("Get all flight ....");
+        LOG.info("Get all flight ....");
         List<Flight> flightList = new ArrayList<>();
         //SQL query for create new flight
         String selectAll = "SELECT * FROM flights;";
@@ -90,26 +105,41 @@ public class FlightDAOImpl implements FlightDAO {
             }
         } catch (SQLException | IOException | ClassNotFoundException e) {
             e.printStackTrace();
+            LOG.warning("Same problem in \"getAllFlight\" method" + this.getClass());
+
         }
         //return all flight
         return flightList;
     }
 
+    /**
+     * Delete flight by id request
+     *
+     * @param id request
+     */
     @Override
     public void deleteFlightByIdRequest(Long id) {
         //SQL query for create new flight
         String deleteFlightByIdRequest = "DELETE FROM flights WHERE request = " + id + ";";
         try (Statement stmt = CONNECT.createStatement();) {
             stmt.executeUpdate(deleteFlightByIdRequest);
-            log.info("User with id == " + id + ", deleted successfully!");
+            LOG.info("User with id == " + id + ", deleted successfully!");
         } catch (SQLException e) {
             e.printStackTrace();
+            LOG.warning("Same problem in \"deleteFlightByIdRequest\" method" + this.getClass());
+
         }
     }
 
+    /**
+     * Finr List<Flight> by user entity
+     *
+     * @param driver entity for find
+     * @return List<Flight>
+     */
     @Override
     public List<Flight> getAllFlightByDriver(User driver) {
-        log.info("Get all flight by driver ....");
+        LOG.info("Get all flight by driver ....");
         List<Flight> flightListByDriver = new ArrayList<>();
         //SQL query for create new flight
         String selectAll = "SELECT * FROM flights;";
@@ -117,7 +147,7 @@ public class FlightDAOImpl implements FlightDAO {
         try (ResultSet rs = smtp.executeQuery(selectAll);) {
             while (rs.next()) {
                 User user = (User) SERIALIZATION.fromString(rs.getString(7));
-                if (driver.getId().equals(user.getId())){
+                if (driver.getId().equals(user.getId())) {
                     Flight newFlight = new Flight();
                     newFlight.setId(rs.getLong(1));
                     newFlight.setEndDate(ZonedDateTime.parse(rs.getString(2)));
@@ -132,28 +162,34 @@ public class FlightDAOImpl implements FlightDAO {
             }
         } catch (SQLException | IOException | ClassNotFoundException e) {
             e.printStackTrace();
+            LOG.warning("Same problem in \"getAllFlightByDriver\" method" + this.getClass());
+
         }
         //return all flight
         return flightListByDriver;
     }
 
+    /**
+     * Change flight status in flight by id
+     * Flight status: "OPEN", "CLOSE", "IN PROGRESS", "CANCELED", "DONE"
+     *
+     * @param id
+     * @param flightStatus
+     */
     @Override
     public void changeFlightStatus(Long id, FlightStatus flightStatus) {
-        log.info("Change flight status on --> \"" + flightStatus.getStatusValue() + "\", by id == " + id + " ....");
+        LOG.info("Change flight status on --> \"" + flightStatus.getStatusValue() + "\", by id == " + id + " ....");
         //SQL query for update car_status car by id
-        String changeCarTechnicelStatusById =
-                "UPDATE flights SET flight_status = ? WHERE id = ?;";
+        String changeFlightStatusById = "UPDATE flights SET flight_status = ? WHERE id = ?;";
         PreparedStatement preparedStmt = null;
         try {
-            preparedStmt = CONNECT.prepareStatement(changeCarTechnicelStatusById);
+            preparedStmt = CONNECT.prepareStatement(changeFlightStatusById);
             preparedStmt.setString(1, flightStatus.getStatusValue());
             preparedStmt.setLong(2, id);
             preparedStmt.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
-            log.warning("Same problem in " + this.getClass());
+            LOG.warning("Same problem in \"changeFlightStatus\" method" + this.getClass());
         }
     }
-
-
 }
