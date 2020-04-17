dd61615c001664cf9d2a094eede67caa507704b3
Kovteba Dima <kovteba@gmail.com>
2020-04-17 10:37:18 +0300

    implemented servlet endDateUp and endDateDown

--- src/main/java/ua/nure/kovteba/finaltask/controller/flight/SortFlightByEndDateUp.java
+++ src/main/java/ua/nure/kovteba/finaltask/controller/flight/SortFlightByEndDateUp.java
@@ -1,4 +1,66 @@
 package ua.nure.kovteba.finaltask.controller.flight;
 
-public class SortFlightByEndDateUp {
+import ua.nure.kovteba.finaltask.dao.token.TokenDAO;
+import ua.nure.kovteba.finaltask.dao.token.TokenDAOImpl;
+import ua.nure.kovteba.finaltask.dao.user.UserDAO;
+import ua.nure.kovteba.finaltask.dao.user.UserDAOImpl;
+import ua.nure.kovteba.finaltask.entity.User;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.logging.Logger;
+
+@WebServlet(
+        name = "sortFlightByEndDateUp",
+        urlPatterns = "/sortFlightByEndDateUp"
+)
+public class SortFlightByEndDateUp   extends HttpServlet {
+
+    //Create logger
+    private static Logger log = Logger.getLogger(SortFlightByEndDateDown.class.getName());
+
+    private static UserDAO userDAO;
+    private static TokenDAO tokenDAO;
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        userDAO = new UserDAOImpl();
+        tokenDAO = new TokenDAOImpl();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String userToken = "0";
+
+        if (req.getSession().getAttribute("userToken") != null){
+            userToken = String.valueOf(req.getSession().getAttribute("userToken"));
+        }
+
+        User user = null;
+        if (!userToken.equals("0")){
+            user = userDAO.getUserById(tokenDAO.getTokenByToken(userToken).getUser());
+
+            if (req.getParameter("flightEndValue") != null){
+                req.getSession().setAttribute("typeSort", req.getParameter("flightEndValue"));
+            }
+        }
+
+        if (userToken.equals("0")){
+            resp.sendRedirect("");
+        } else if (user.getRole().getRoleValue().equals("ADMIN")){
+            resp.sendRedirect("admin");
+        } else if (user.getRole().getRoleValue().equals("DRIVER")){
+            resp.sendRedirect("driver");
+        } else if (user.getRole().getRoleValue().equals("DISPATCHER")){
+            resp.sendRedirect("dispatcher");
+        }
+
+    }
+
 }
