caad4687473e2a6d3df6aa7697237af53f08dd06
Kovteba Dima <kovteba@gmail.com>
2020-04-17 18:35:48 +0300

    Add missed test to EmploymentStatusDAOImplTest

--- src/test/java/ua/nure/kovteba/finaltask/dao/employmentstatus/EmploymentStatusDAOImplTest.java
+++ src/test/java/ua/nure/kovteba/finaltask/dao/employmentstatus/EmploymentStatusDAOImplTest.java
@@ -1,39 +1,50 @@
 package ua.nure.kovteba.finaltask.dao.employmentstatus;
 
 import org.junit.jupiter.api.Test;
+import ua.nure.kovteba.finaltask.entity.EmploymentStatus;
 import ua.nure.kovteba.finaltask.enumlist.Employment;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class EmploymentStatusDAOImplTest {
 
-    private static EmploymentStatusDAOImpl employmentStatusDAO;
+    private static final EmploymentStatusDAOImpl EMPLOYMENT_STATUS_DAO;
 
     static {
-        employmentStatusDAO = new EmploymentStatusDAOImpl();
+        EMPLOYMENT_STATUS_DAO = new EmploymentStatusDAOImpl();
     }
 
     @Test
     void createEmploymentStatus() {
-        employmentStatusDAO.createEmploymentStatus(75L, Employment.FREE);
+        EMPLOYMENT_STATUS_DAO.createEmploymentStatus(151L, Employment.FREE);
     }
 
     @Test
     void changeEmploymentStatus() {
-        employmentStatusDAO.changeEmploymentStatus(75L, Employment.FREE);
+        EMPLOYMENT_STATUS_DAO.changeEmploymentStatus(151L, Employment.FREE);
     }
 
     @Test
     void getAllFreeDrivers() {
-        List<Long> list = employmentStatusDAO.getAllFreeDrivers();
+        List<Long> list = EMPLOYMENT_STATUS_DAO.getAllFreeDrivers();
         if (list.size() == 0){
             System.out.println("Value : " + list.size());
         } else {
             for (Long l : list){
                 System.out.println(l);
             }
+        }
+    }
+
+    @Test
+    void deleteEmploymentStatusByDriverId() {
+        EMPLOYMENT_STATUS_DAO.deleteEmploymentStatusByDriverId(151L);
+    }
+
+    @Test
+    void getAllValueEmployment() {
+        for (EmploymentStatus employmentStatus : EMPLOYMENT_STATUS_DAO.getAllValueEmployment()){
+            System.out.println(employmentStatus.toString());
         }
     }
 }
