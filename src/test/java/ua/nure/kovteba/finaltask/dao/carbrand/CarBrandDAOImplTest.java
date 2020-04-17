d6b71483c54ff65c8e0436848075a8a441d6dcb1
Kovteba Dima <kovteba@gmail.com>
2020-04-17 18:16:15 +0300

    Clean CarDAOImplTest.java, add missied test

--- src/test/java/ua/nure/kovteba/finaltask/dao/carbrand/CarBrandDAOImplTest.java
+++ src/test/java/ua/nure/kovteba/finaltask/dao/carbrand/CarBrandDAOImplTest.java
@@ -5,25 +5,41 @@ import ua.nure.kovteba.finaltask.entity.CarBrand;
 
 class CarBrandDAOImplTest {
 
-    private static CarBrandDAOImpl carBrandDAO = new CarBrandDAOImpl();
+    private static final CarBrandDAOImpl CAR_BRAND_DAO;
+
+    static {
+        CAR_BRAND_DAO = new CarBrandDAOImpl();
+    }
 
     @Test
-    void cteateCarBrand() {
+    void createCarBrand() {
         CarBrand carBrand = new CarBrand();
-        carBrand.setBrandName("Mersedes Benz");
-        Long id = carBrandDAO.createCarBrand(carBrand);
+        carBrand.setBrandName("TestValue");
+        Long id = CAR_BRAND_DAO.createCarBrand(carBrand);
         System.out.println(id);
     }
 
     @Test
     void getCarBrandById() {
         System.out.println("getCarBrandById");
-        System.out.println(carBrandDAO.getCarBrandById(3L).toString());
+        System.out.println(CAR_BRAND_DAO.getCarBrandById(31L).toString());
     }
 
     @Test
     void getCarBrandByBrandValue() {
         System.out.println("getCarBrandByBrandValue");
-        System.out.println(carBrandDAO.getCarBrandByBrandValue("MAN"));
+        System.out.println(CAR_BRAND_DAO.getCarBrandByBrandValue("TestValue"));
+    }
+
+    @Test
+    void getAllCarBrand() {
+        for (CarBrand carBrand : CAR_BRAND_DAO.getAllCarBrand()){
+            System.out.println(carBrand.toString());
+        }
+    }
+
+    @Test
+    void deleteCarBrand() {
+        CAR_BRAND_DAO.deleteCarBrand(31L);
     }
 }
